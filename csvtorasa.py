d485db45cf458a3da37aa4b50b1e5e5c42a99ecd
lohithpro <lohith.arcot@gmail.com>
2019-11-29 16:01:31 +0530

    creates a domain.yml file as well

--- csvtorasa.py
+++ csvtorasa.py
@@ -1,5 +1,7 @@
 import pandas as pd
-def create_rasa_nlu(path, nlu_md_path):
+
+
+def create_rasa_files(path, nlu_md_path):
     """
     Converts an CSV file created with the specified format to RASA accepted nlu.md format
     :param path: path where the CSV file is present
@@ -10,10 +12,16 @@ def create_rasa_nlu(path, nlu_md_path):
     file = open(r'{}\nlu.md'.format(nlu_md_path), "w")
     intents = list(df.columns)
     for item in intents:
-        file.write("## intent: {intent_name}\n".format(intent_name = item))
+        file.write("## intent: {intent_name}\n".format(intent_name=item))
         for sent in df[item]:
             file.write("- {}\n".format(sent))
     file.close()
+    file = open(r'{}\domain.yml'.format(nlu_md_path), "w")
+    file.write("intents:\n")
+    for intent_name in intents:
+        file.write("  - {}\n".format(intent_name))
+    file.write("actions:\n")
+    for intent_name in intents:
+        file.write("  - utter_{}\n".format(intent_name))
+    file.close()
     return None
-
-
