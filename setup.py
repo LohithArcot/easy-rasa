0704e738d17d429e1950118118f41e8acf2df54f
Lohith Arcot <lohith.arcot@gmail.com>
2024-06-12 15:59:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
+from pathlib import Path
+
 from setuptools import setup, find_packages
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 setup(
-    name="csvtorasa",
-    version="0.1.1",
+    name="easy-rasa",
+    version="1.0",
     author='Lohith Arcot',
     author_email='lohith.arcot@gmail.com',
     description='A package that converts .csv file to rasa compatible file',
-    long_description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -14,7 +19,7 @@ setup(
     ],
     entry_points={
         # "console_scripts":[
-        #     "csvtorasa = csvtorasa:create_rasa_files"
+        #     "easy-rasa = easy-rasa:create_rasa_files"
         # ]
     },
 )
